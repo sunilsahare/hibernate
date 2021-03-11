34e7cd6f4a19fb4d02815eb263ffe21f141b1cf0
Sunil Sahare <sunilsahare111@gmail.com>
2021-03-11 23:01:34 +0530

    Add try catch Block

--- One to One/src/com/techeasy/main/ReadApp.java
+++ One to One/src/com/techeasy/main/ReadApp.java
@@ -7,7 +7,7 @@ import org.hibernate.cfg.Configuration;
 import com.techeasy.entity.User;
 import com.techeasy.entity.UserInfo;
 
-public class CreateApp {
+public class ReadApp {
 
 	public static void main(String[] args) {
 		
@@ -19,16 +19,26 @@ public class CreateApp {
 		
 		Session session = factory.openSession();
 		
-		int id=1;
-		//retrieve User Object
-		User user = session.get(User.class, id);
+		try{
+			
+			int id=1;
+			//retrieve User Object
+			User user = session.get(User.class, id);
+			
+			System.out.println("User:"+user);
+			System.out.println("Associated Object User Info:"+user.getUserInfo());
+			
+		}
+		catch(Exception e){
+			System.out.println("Error: "+e);
+		}
+		finally{
+			//close factory
+			factory.close();
+		}
+
 		
-		System.out.println("User:"+user);
-		System.out.println("Associated Object User Info:"+user.getUserInfo());
 		
-		
-		//close factory
-		factory.close();
 		
 	}
 }
